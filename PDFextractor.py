6c5b8794f97c7617aad7cba3e954ddaf2e9bb4d7
DragonQueen <vworri@gmail.com>
2017-05-27 14:07:35 -0400

    Adding the PDF parser to the repo.

---
+++ PDFextractor.py
@@ -0,0 +1,31 @@
+from pdfminer.converter import TextConverter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+
+from cStringIO import StringIO
+
+
+def convert(fname, pages=None):
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+
+    output = StringIO()
+    manager = PDFResourceManager()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    infile = file(fname, 'rb')
+    for page in PDFPage.get_pages(infile, pagenums):
+        interpreter.process_page(page)
+    infile.close()
+    converter.close()
+    text = output.getvalue()
+    output.close()
+    print (text)
+    return text
+
+convert("sample.pdf")
