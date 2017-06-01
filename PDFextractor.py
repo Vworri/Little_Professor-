d9fef218ea177cacb5815e9ccdba07652a953a8b
DragonQueen <vworri@gmail.com>
2017-06-01 14:59:26 -0400

    transfer to Python 3 and test pdf with images

--- PDFextractor.py
+++ PDFextractor.py
@@ -1,31 +1,34 @@
-from pdfminer.converter import TextConverter
-from pdfminer.pdfpage import PDFPage
-from pdfminer.layout import LAParams
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
+from memory_profiler import profile
 
-from cStringIO import StringIO
+class extract_text(object):
+    def __init__(self):
+        text = self.convert("AHistoryOfTheUnitedStatesVol2.pdf")
+        with open("testFile.txt", "w+") as f:
+            f.write(text)
+    def convert(self,fname, pages=None):
+        text = ""
+        ifp = open(fname, 'rb')
+        parser = PDFParser(ifp)
+        doc = PDFDocument()
+        parser.set_document(doc)
+        doc.set_parser(parser)
+        doc.initialize('')
+        rsrcmgr = PDFResourceManager()
+        laparams = LAParams()
+        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        # Process each page contained in the document.
+        for page in doc.get_pages():
+            interpreter.process_page(page)
+            layout = device.get_result()
+            for lt_obj in layout:
+                if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+                    text += lt_obj.get_text()
+        return text
 
-
-def convert(fname, pages=None):
-    if not pages:
-        pagenums = set()
-    else:
-        pagenums = set(pages)
-
-    output = StringIO()
-    manager = PDFResourceManager()
-    converter = TextConverter(manager, output, laparams=LAParams())
-    interpreter = PDFPageInterpreter(manager, converter)
-
-    infile = open(fname, 'rb')
-    for page in PDFPage.get_pages(infile, pagenums):
-        interpreter.process_page(page)
-    infile.close()
-    converter.close()
-    text = output.getvalue()
-    output.close()
-    print (text)
-    return text
-
-convert("sample.pdf")
+if "__main__"==__name__:
+    extract_text()
